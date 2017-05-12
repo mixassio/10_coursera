fa73e6eb750912ad8fe5c3b8f318046cf01f5346
mixassio <mixail.bondarev@gmail.com>
2017-05-12 16:05:08 +0300

    ddd

--- coursera.py
+++ coursera.py
@@ -1,7 +1,18 @@
-
+import xml.etree.ElementTree as etree
+import requests
+import json
+import re
 
 def get_courses_list():
-    pass
+    tree = etree.parse('./coursera.xml')
+    root = tree.getroot()
+    my_dict = []
+    for i, k in enumerate(root):
+        for j in range(0, len(k)):
+            result = re.sub('\n', '', k[j].text)
+            my_dict.append(result) # красивый список всех ссылок
+    list_20 = [my_dict[i] for i in range(15,35)]
+    return list_20
 
 
 def get_course_info(course_slug):
@@ -13,4 +24,4 @@ def output_courses_info_to_xlsx(filepath):
 
 
 if __name__ == '__main__':
-    pass
+    get_courses_list()
